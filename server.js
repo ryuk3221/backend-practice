7f672a77826fcca2b8f1a2468c78b3c8f17393d7
Danil <danilmitrofanov123@gmail.com>
2025-06-02 18:59:40 +0300

    start

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import authRoutes from "./app/auth/auth.routes.js";
+import 'dotenv/config';
+import morgan from "morgan";
+import { prisma } from "./app/prisma.js";
+
+const app = express();
+
+async function main() {
+  if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+  app.use(express.json());
+  app.use('/api/auth', authRoutes);
+
+  const PORT = 5000;
+
+  app.listen(
+    PORT,
+    console.log('Server running')
+  );
+}
+
+main().then(async () => {
+  await prisma.$disconnect();
+})
+  .catch(async e => {
+    console.log(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
